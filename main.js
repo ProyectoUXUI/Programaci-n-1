9343352681db3d5234da524acf87349e4f51d871
Alumno03 J901 <a03j901@campustls.local>
2019-01-10 20:51:14 -0500

    Cambios en el boton de comprar

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const botonComprar = document.querySelectorAll(".boton-comprar")[0];
-  botonComprar.addEventListener("click", function() {
-    alert("Gracias por tu compra");
+  // const botonComprar = document.querySelectorAll(".boton-comprar")[0];
+  // botonComprar.addEventListener("click", function() {
+  //   alert("Gracias por tu compra");
+  // })
+  const form = document.querySelectorAll(".form")[0];
+  form.addEventListener("submit", function() {
+    const cantidad = document.querySelectorAll(".cantidad")[0].value;
+    alert(`Gracias por comprar ${cantidad} de ...`);
   })
 });
